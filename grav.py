f117f0a8e024adf70eb425425dda6ba2c6d4f022
James Hemming <jkhemming@jkhemming.co.uk>
2012-07-27 00:56:47 +0100

    Fixed some bad naming.

--- grav.py
+++ grav.py
@@ -7,8 +7,8 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("ship.png")
-ballrect = ball.get_rect()
+ship = pygame.image.load("ship.png")
+shiprect = ship.get_rect()
 
 while 1:
 	for event in pygame.event.get():
@@ -17,18 +17,18 @@ while 1:
 	key = pygame.key.get_pressed()
 	
 	if key[pygame.K_DOWN]:
-		ballrect = ballrect.move(speed)
+		shiprect = shiprect.move(speed)
 		speed = [0, 1]
 	if key[pygame.K_RIGHT]:
-		ballrect = ballrect.move(speed)
+		shiprect = shiprect.move(speed)
 		speed = [1, 0]
 	if key[pygame.K_UP]:
-		ballrect = ballrect.move(speed)
+		shiprect = shiprect.move(speed)
 		speed = [0, -1]
 	if key[pygame.K_LEFT]:
-		ballrect = ballrect.move(speed)
+		shiprect = shiprect.move(speed)
 		speed = [-1, 0]
 	
 	screen.fill(black)
-	screen.blit(ball, ballrect)
+	screen.blit(ship, shiprect)
 	pygame.display.flip()
