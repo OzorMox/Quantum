172b517d6c94f3a7aa50a601f1aab300f33e91c8
Dean Harris <deanharris07@gmail.com>
2012-08-03 00:53:16 +0100

    Smashed existing code into a classy class

--- quantum.py
+++ quantum.py
@@ -1,34 +1,57 @@
 import sys, pygame
-pygame.init()
 
-size = width, height = 1024, 768
-speed = [1, 1]
-black = 0, 0, 0
+class Quantum:
+    def __init__(self):
+        pygame.init()
+        size = width, height = 1024, 768
+        self.screen = pygame.display.set_mode(size)
 
-screen = pygame.display.set_mode(size)
 
-ship = pygame.image.load("ship.png")
-shiprect = ship.get_rect()
+    def load(self):                
+        self.ship = pygame.image.load("ship.png")
+        self.shiprect = self.ship.get_rect()
 
-while 1:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-		
-	key = pygame.key.get_pressed()
-	
-	if key[pygame.K_DOWN]:
-		shiprect = shiprect.move(speed)
-		speed = [0, 1]
-	if key[pygame.K_RIGHT]:
-		shiprect = shiprect.move(speed)
-		speed = [1, 0]
-	if key[pygame.K_UP]:
-		shiprect = shiprect.move(speed)
-		speed = [0, -1]
-	if key[pygame.K_LEFT]:
-		shiprect = shiprect.move(speed)
-		speed = [-1, 0]
-	
-	screen.fill(black)
-	screen.blit(ship, shiprect)
-	pygame.display.flip()
+        
+    def run(self):
+        while 1:
+            self.update()
+            self.draw()
+
+
+    def update(self):
+        self.updateInput()
+
+
+    def updateInput(self):
+        speed = [1, 1]
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+                
+        key = pygame.key.get_pressed()
+        
+        if key[pygame.K_DOWN]:
+            speed = [0, 1]
+            self.shiprect = self.shiprect.move(speed)
+        if key[pygame.K_RIGHT]:
+            speed = [1, 0]
+            self.shiprect = self.shiprect.move(speed)
+        if key[pygame.K_UP]:
+            speed = [0, -1]
+            self.shiprect = self.shiprect.move(speed)
+        if key[pygame.K_LEFT]:
+            speed = [-1, 0]
+            self.shiprect = self.shiprect.move(speed)
+
+
+    def draw(self): 
+        black = 0, 0, 0
+        
+        self.screen.fill(black)
+        self.screen.blit(self.ship, self.shiprect)
+        pygame.display.flip()
+
+
+quantum = Quantum()
+quantum.load()
+quantum.run()
